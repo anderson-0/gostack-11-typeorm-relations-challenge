39203590526e47b3b1cb2dfdadde7839b7327eed
Anderson Mattjie da Silva <anderson.consultant.it@outlook.com>
2021-02-22 14:17:42 -0300

    feat: implemented CreateCustomerService service with controller

--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -12,10 +12,21 @@ interface IRequest {
 
 @injectable()
 class CreateCustomerService {
-  constructor(private customersRepository: ICustomersRepository) {}
+  constructor(
+    @inject('CustomersRepository')
+    private customersRepository: ICustomersRepository,
+  ) {}
 
   public async execute({ name, email }: IRequest): Promise<Customer> {
-    // TODO
+    const costumerExists = await this.customersRepository.findByEmail(email);
+
+    if (costumerExists) {
+      throw new AppError('This e-mail is already in use.');
+    }
+
+    const customer = await this.customersRepository.create({ name, email });
+
+    return customer;
   }
 }
 
